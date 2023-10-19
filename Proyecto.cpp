3bbcb99ac28d12c2f31af34e68fdeb3e396de1de
brand <brandondelapaz@hotmail.com>
2023-10-18 18:45:47 -0600

    Se sube el cpp

---
+++ Proyecto.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+#define fore(i,a,b)  for(int i(1);i<=a;i=i+b)
+#define forec(i,a,b) for(int i(0);i<a;i=i+b)
+#define forel(i,a,b)  for(lli i(1);i<=a;i=i+b)
+#define forecl(i,a,b) for(lli i(0);i<a;i=i+b)
+#define lli long long int
+#define endL '\n'
+
+using namespace std;
+
+lli fac(lli);
+
+int main() {
+    lli n;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin>>n;
+    cout<<fac(n)<<endL;
+    }
+
+lli fac(lli n) {
+    int i(0);
+
+    n*=i;
+
+    return n;
+}
